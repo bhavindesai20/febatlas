e36b2fef8c6a0aa95df8adda0e507c6314c953d0
surya <surya.mylar@gmail.com>
2016-02-23 16:52:06 -0500

    feat(userService): Implemented userService

--- src/com/atlas/service/UserService.java
+++ src/com/atlas/service/UserService.java
@@ -10,9 +10,9 @@ import com.atlas.entity.User;
 public interface UserService {
 	public void addUser(User u);
 
-	public void updateUser(User u);
+	public User updateUser(User u);
 
-	public void removeUser(int id);
+	public User removeUser(int id);
 
 	public List<User> listUser();
 
