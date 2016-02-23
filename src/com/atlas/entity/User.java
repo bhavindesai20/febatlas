e0b91d7406915725b7a64a1cb3799851b6688ae7
surya <surya.mylar@gmail.com>
2016-02-23 14:35:17 -0500

    feat(userService): updated user entity

--- src/com/atlas/entity/User.java
+++ src/com/atlas/entity/User.java
@@ -1,14 +1,33 @@
 package com.atlas.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.springframework.stereotype.Component;
 
-
 @Component
+@Entity
+@Table(name = "User")
 public class User {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
-	private String userName;
+
+	@Column(name = "firstName", nullable = false)
+	private String firstName;
+
+	@Column(name = "firstName")
+	private String LastName;
+
+	@Column(name = "firstName", unique = true, nullable = false)
 	private String email;
+
+	@Column(name = "firstName", nullable = false)
 	private String password;
 
 	public int getId() {
@@ -19,12 +38,20 @@ public class User {
 		this.id = id;
 	}
 
-	public String getUserName() {
-		return userName;
+	public String getFirstName() {
+		return firstName;
 	}
 
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
 	}
 
 	public String getEmail() {
@@ -42,5 +69,7 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	
 
 }
