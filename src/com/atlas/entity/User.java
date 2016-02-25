66630db951d752abb1ad73a936eb4e9a4e831f28
bhavindesai20 <bhavindesai20@gmail.com>
2016-02-24 23:59:29 -0600

    feat(user-service) : change pojo for other table compatibility.

--- src/com/atlas/entity/User.java
+++ src/com/atlas/entity/User.java
@@ -1,17 +1,26 @@
 package com.atlas.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 @Component
 @Entity
 @Table(name = "User")
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+@JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,6 +38,36 @@ public class User {
 
 	@Column(name = "password", nullable = false)
 	private String password;
+	
+	@OneToMany(mappedBy = "user")
+	private List<Comments> comments;
+	
+	@OneToMany(mappedBy = "user")
+	private List<Rating> rating;
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Comments> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<Comments> comments) {
+		this.comments = comments;
+	}
+
+	public List<Rating> getRating() {
+		return rating;
+	}
+
+	public void setRating(List<Rating> rating) {
+		this.rating = rating;
+	}
 
 	public int getId() {
 		return id;
